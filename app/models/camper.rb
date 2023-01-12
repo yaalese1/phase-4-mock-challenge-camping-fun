4d595bc37853b7a23e0a686f1240cd808d0f3ae1
Yetunde Alese <yaalese1@gmail.com>
2023-01-12 13:13:14 -0500

    validate camper and validate signup

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -2,7 +2,11 @@ class Camper < ApplicationRecord
     has_many :signups
     has_many :activities, through: :signups
 
-    # validates :name, presence: true
-    # validates :age, length: {in: 8..18}
+    validates :name, presence: true
+    validates :age, inclusion:{in: 8..18} ,numericality: true 
+    # validates :age, numericality:{greater_than_or_equal_to: 8, less_than_or_equal_to: 18}
+    # validates :age, comparison:{greater_than_or_equal_to: 8 , less_than_or_equal_to: 18}
+    # validates :age, length: {minimum: 8, maximum:18} 
+    # validates :age, length: {in: 8...18}
 
 end
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,4 +1,17 @@
 class Signup < ApplicationRecord
   belongs_to :camper
   belongs_to :activity
+  validates :time, inclusion:{in: 0..23}, numericality: true 
+  # validates :time, numericality:{in: 0..23}
+  # validate :time_range
 end
+
+# private
+
+# def time_range
+  
+#  if S.time < 0 or Signup.time > 23
+
+#   errors.add(:time,"time must be between 0 and 23")
+#  end
+# end 
