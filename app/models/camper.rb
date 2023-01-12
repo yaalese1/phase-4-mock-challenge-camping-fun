b4638692489b522b0371ebfd299a824cb3a80932
Kevin Dutzy <kddutz15@gmail.com>
2023-01-12 11:56:06 -0500

    added validations

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -2,7 +2,7 @@ class Camper < ApplicationRecord
     has_many :signups
     has_many :activities, through: :signups
 
-    # validates :name, presence: true
-    # validates :age, length: {in: 8..18}
+    validates :name, presence: true
+    validates :age, length: {in: 8..18}
 
 end
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,4 +1,6 @@
 class Signup < ApplicationRecord
   belongs_to :camper
   belongs_to :activity
+
+  validates :time, length: {in: 0..23}
 end
